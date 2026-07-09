8ac6049b109bf4cf38e10632dc1af271efbdbaec
annBuho <annbuho@gmail.com>
2026-07-10 01:44:25 +0300

    Revert "change test login data"
    This reverts commit c06ec6dcbe222a6b2c798c3645c346a8fda36071.

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -4,8 +4,8 @@ const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
-    email: 'user@testmail.com',
-    password: '098765',
+    email: 'user@nextmail.com',
+    password: '123456',
   },
 ];
 
